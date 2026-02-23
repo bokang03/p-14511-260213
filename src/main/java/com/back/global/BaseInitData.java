94383c85b1b245054f33ab2f9080b6c503892235
bokang03 <bogang12345@gmail.com>
2026-02-23 12:47:37 +0900

    refactor : 트랜잭션 적용

--- src/main/java/com/back/global/BaseInitData.java
+++ src/main/java/com/back/global/BaseInitData.java
@@ -2,14 +2,20 @@ package com.back.global;
 
 import com.back.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
 @RequiredArgsConstructor
 public class BaseInitData {
 
+    @Autowired
+    @Lazy
+    private BaseInitData self;
 
     private final PostService postService;
 
@@ -17,19 +23,28 @@ public class BaseInitData {
     ApplicationRunner initDataRunner() {
         return args -> {
 
-            work1();
-            work2();
+            new Thread(() -> {
+                self.work1();
+            }).start();
 
+            self.work2();
         };
     }
 
+    @Transactional
     void work1() {
 
-        if(postService.count() > 0) {
+        if (postService.count() > 0) {
             return;
         }
 
+        // 어떤 기능을 테스트 데이터가 2개인 것을 가정하고 개발
         postService.write("제목1", "내용1");
+
+//        if (true) {
+//            throw new RuntimeException("테스트 예외");
+//        }
+
         postService.write("제목2", "내용2");
     }
 
@@ -37,5 +52,4 @@ public class BaseInitData {
         postService.findById(1);
         // select * from post where id = 1;
     }
-
 }
