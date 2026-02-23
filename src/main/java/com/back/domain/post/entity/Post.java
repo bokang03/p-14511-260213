51e1053ff68e21e7cb7b44b9d94dfedee29a341c
bokang03 <bogang12345@gmail.com>
2026-02-23 15:46:58 +0900

    feat : 모든 엔터티의 기본 속성을 BaseEntity 클래스에 모아두고 상속받아서 사용.

--- src/main/java/com/back/domain/post/entity/Post.java
+++ src/main/java/com/back/domain/post/entity/Post.java
@@ -1,37 +1,22 @@
 package com.back.domain.post.entity;
 
+import com.back.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
 @Setter
 @Getter
-@EntityListeners(AuditingEntityListener.class)
-public class Post {
-
-    @Id // 기본키
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // autoIncrement
-    private int id;
+public class Post extends BaseEntity {
 
     @Column(length = 100, nullable = false) // varchar(100), NotNull
     private String title;
 
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
-
-    @CreatedDate
-    private LocalDateTime createDate;
-
-    @LastModifiedDate
-    private LocalDateTime modifyDate;
 
 
     public Post(String title, String content){
