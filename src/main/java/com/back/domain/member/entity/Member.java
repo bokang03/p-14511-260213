5c126c1fee5b468a7c300c0d99f98bb7949cd01e
bokang03 <bogang12345@gmail.com>
2026-02-24 12:26:20 +0900

    feat :  회원 도메인 패키지 생성

---
+++ src/main/java/com/back/domain/member/entity/Member.java
@@ -0,0 +1,22 @@
+package com.back.domain.member.entity;
+
+import com.back.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+public class Member extends BaseEntity {
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(length = 100)
+    private String password;
+
+    @Column(length = 30)
+    private String nickname;
+}
