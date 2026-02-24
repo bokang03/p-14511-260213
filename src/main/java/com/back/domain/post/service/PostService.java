fc595fc709bc09d4a8be06009b6c523b5e8b5192
bokang03 <bogang12345@gmail.com>
2026-02-24 12:46:13 +0900

    feat :  Post에 authorId 필드를 추가. post의 작성자가 누구인지 알 수 있게 함.

--- src/main/java/com/back/domain/post/service/PostService.java
+++ src/main/java/com/back/domain/post/service/PostService.java
@@ -4,37 +4,41 @@ import com.back.domain.post.entity.Post;
 import com.back.domain.post.repositoty.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-@RequiredArgsConstructor
+@RequiredArgsConstructor// @Component랑 같은 기능
 public class PostService {
 
     private final PostRepository postRepository;
 
-    public Post write(String title, String content){
+    public Post write(int authorId, String title, String content) {
 
-        Post post = new Post(title, content);
+        Post post = new Post(authorId, title, content);
         return postRepository.save(post);
     }
 
-    public Post modify(Post post, String title, String content){
-        post.setTitle(title);
-        post.setContent(content);
+    @Transactional
+    public Post modify(Post post, String newTitle, String newContent) {
+        post.setTitle(newTitle);
+        post.setContent(newContent);
+//        return postRepository.save(post);
         return post;
     }
 
-    public Optional<Post> findById(int id){
+    public Optional<Post> findById(int id) {
         return postRepository.findById(id);
     }
 
-    public List<Post> findAll(){
+    public List<Post> findAll() {
         return postRepository.findAll();
     }
 
-    public long count(){
+    public long count() {
         return postRepository.count();
     }
+
 }
