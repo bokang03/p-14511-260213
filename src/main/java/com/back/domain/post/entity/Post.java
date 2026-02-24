cddee209f5b410808869cd8476d1a121363d3f0a
bokang03 <bogang12345@gmail.com>
2026-02-24 14:39:42 +0900

    feat : JPA 방식 외래키 작성

--- src/main/java/com/back/domain/post/entity/Post.java
+++ src/main/java/com/back/domain/post/entity/Post.java
@@ -1,7 +1,10 @@
 package com.back.domain.post.entity;
 
+import com.back.domain.member.entity.Member;
 import com.back.global.entity.BaseEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -18,11 +21,12 @@ public class Post extends BaseEntity {
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
 
-    private int authorId;
+    @ManyToOne
+    private Member author;
 
 
-    public Post(int authorId, String title, String content){
-        this.authorId = authorId;
+    public Post(Member author, String title, String content){
+        this.author = author;
         this.title = title;
         this.content = content;
     }
